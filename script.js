4526bfe2680a59a7828ceaf93b0c4d9c3d6cd1ab
GummyRicez-AR <adarshram2101@gmail.com>
2024-10-20 01:47:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+var friskXPos = 0;
+var image = document.createElement("img");
+
 document.body.onload = () => {
-    var image = document.createElement("img");
     image.src = chrome.runtime.getURL("graphics/SOU_Frisk.png");
     image.style.width = "200px";
     image.style.height = "300px";
     image.style.position = "fixed";
-    image.style.bottom = 0;
-    image.style.left = 0;
+    image.style.bottom = "0px";
+    image.style.left = "0px";
     document.body.append(image);
+    moveFrisk();
 }
 
 const elements = document.querySelectorAll("p");
@@ -18,4 +21,34 @@ elements.forEach(element => {
 
 if (bigHeader) {
     bigHeader.style.color = "red";
+}
+
+function moveFrisk() {
+    var friskTargetX = Math.floor(Math.random() * 1000);
+    console.log(friskTargetX);
+    if (friskXPos < friskTargetX) {
+        console.log("yes");
+        interval = setInterval( () => {
+            console.log("interval+");
+            friskXPos += 2;
+            image.style.left = friskXPos.toString() + "px";
+            if (friskXPos >= friskTargetX) {
+                image.style.left = friskTargetX.toString() + "px";
+                friskXPos = friskTargetX;
+                clearInterval(interval);
+            }
+        }, 20);
+    } else {
+        console.log("no");
+        interval = setInterval( () => {
+            console.log("interval-");
+            friskXPos -= 2; 
+            image.style.left = friskXPos.toString() + "px";
+            if (friskXPos <= friskTargetX) {
+                image.style.left = friskTargetX.toString() + "px";
+                friskXPos = friskTargetX;
+                clearInterval(interval);
+            }
+        }, 20);
+    }
 }
